1407c8120b149478eec79ae22c7df74925fbd123
lessthandot <lessthandot@CentOS-65-64-minimal.(none)>
2019-03-16 23:31:00 +0100

    All the things

---
+++ wp-atom.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=/index.php/feed/atom/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/index.php/feed/atom/";
+		</script>
+
+		<p>You are being redirected to <a href="/index.php/feed/atom/">/index.php/feed/atom/</a></p>
+	</body>
+</html>
